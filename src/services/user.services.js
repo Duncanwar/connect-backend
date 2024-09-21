baa809e2d8b051b2d500992a5110b9d5b8eec0d0
Ndanyuzwe Duncan <semunda9@gmail.com>
2024-09-21 23:38:29 +0200

    new

--- src/services/user.services.js
+++
@@ -1,9 +0,0 @@
-const UserModel = mongoose.model("../models/user");
-
-export default class UserService {
-  static async create() {}
-
-  static async delete(id) {
-    return await UserModel.findByIdAndDelete(id);
-  }
-}
