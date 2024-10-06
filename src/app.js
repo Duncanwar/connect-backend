3b50952a7b7e3939cc0e1579c884b6c9d81c98f4
Ndanyuzwe Duncan <semunda9@gmail.com>
2024-10-06 23:50:15 +0200

    remove

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ require("./v1/config/dbConfig");
 const routes = require("./v1/config/routes/index.route");
 const socketIO = require("socket.io");
 const http = require("http");
-// require("./cacheManager");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -15,16 +14,6 @@ app.use(cors());
 app.use(routes);
 
 const server = http.createServer(app);
-// const io = socketIO(server, {
-//     cors: {
-//      origin: "http://localhost:3000",
-//      method: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-//      credentials: true,
-//      allowedHeaders: ["Authorization"]
-//     }
-// }
-// );
-// app.set("io", io);
 
 app.get("/", (req, res) => res.json({ message: "done" }));
 
