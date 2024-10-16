bae29efc6ad665c32ddc91cd2e039bdd541bf78c
Duncanwar <semunda9@gmail.com>
2024-10-16 17:29:53 +0200

    post controller

--- src/v1/services/postService.js
+++ src/v1/services/postService.js
@@ -4,11 +4,13 @@ const createNewPost = async (post) => {
   return await PostModel.create(post);
 };
 
-const getAllPosts = async () => {
+const getAllPosts = async (page = 1, limit = 10) => {
   return await PostModel.find()
     .populate("postedBy", "_id name")
     .populate("comments.postedBy", "_id name")
-    .sort("-createdAt");
+    .sort("-createdAt")
+    .skip((page - 1) * limit)
+    .limit(limit);
 };
 
 module.exports = {
