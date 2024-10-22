f5e8520322e3738bf31a31b504f5fb00abaf437a
Duncanwar <semunda9@gmail.com>
2024-10-22 19:00:31 +0200

    remove the switch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import cors from "cors";
 import express from "express";
 
-import "./v1/config/dbConfig.js";
+import "./v1/config/dbConfig.ts";
 import route from "./v1/routes/index.js";
 
 const app = express();
@@ -11,8 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use("/api/v1", route);
-
-app.get("/", (req, res) => res.json({ message: "done" }));
 
 app.listen(PORT, () => {
   console.log("server is running on,PORT", PORT);
